281c23262aa141267c62134c521f95dc3b447d62
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-04-20 11:36:30 +0200

    Utworzenie bazy danych

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using DbReposotiries;
+using IRepositories;
+using Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +16,25 @@ namespace ConsoleClient
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+
+            AddCustomerTest();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+
+        private static void AddCustomerTest()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ShopConnectionString"].ConnectionString;
+            DbConnection connection = new SqlConnection(connectionString);
+            ShopContext context = new ShopContext(connection, contextOwnsConnection: false);
+
+            Customer customer = new Customer { FirstName = "John", LastName = "Smith", Gender = Gender.Male };
+
+            ICustomerRepository customerRepository = new DbCustomerRepository(context);
+            customerRepository.Add(customer);
         }
     }
 }
