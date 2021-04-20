c1e3906d2b21655be2862a5e66d9e001943cbfd1
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-04-20 10:33:38 +0200

    Utworzenie repozytorium

---
+++ DbReposotiries/DbCustomerRepository.cs
@@ -0,0 +1,47 @@
+﻿using IRepositories;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbReposotiries
+{
+
+    public class DbCustomerRepository : ICustomerRepository
+    {
+        private readonly ShopContext context;
+
+        public DbCustomerRepository(ShopContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(Customer customer)
+        {
+            context.Customers.Add(customer); // <- dodanie obiektu do Contextu
+            context.SaveChanges();           // <- wysłanie zapytania SQL do bazy danych
+        }
+
+        public IEnumerable<Customer> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DbReposotiries/ShopContext.cs
@@ -0,0 +1,31 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbReposotiries
+{
+
+    // PM> Install-Package EntityFramework
+    public class ShopContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Service> Services { get; set; }
+
+        public ShopContext(DbConnection connection, bool contextOwnsConnection) 
+            : base(connection, contextOwnsConnection)
+        {
+
+        }
+
+
+    }
+
+    // Materializacja = recordset -> obiekty
+}
