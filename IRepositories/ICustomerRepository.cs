ee37db94b47d44190a563dada1336ea2cd7a8796
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-04-20 10:33:27 +0200

    Utworzenie interfejsów

---
+++ IRepositories/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+
+namespace IRepositories
+{
+    public interface ICustomerRepository
+    {
+        void Add(Customer customer);
+        void Update(Customer customer);
+        void Remove(int id);
+        IEnumerable<Customer> Get();
+        Customer Get(int id);
+    }
+}
