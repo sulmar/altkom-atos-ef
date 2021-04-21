52ee27356d94f5017eeb36883d272174bcb59e39
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-04-21 16:21:08 +0200

    Rysunki

---
+++ DbReposotiries/Extensions/DbContextExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbReposotiries.Extensions
+{
+    public static class DbContextExtensions
+    {
+        public static DbContext BulkInsert<T>(this DbContext context, T entity, int count, int batchSize)
+            where T : class
+        {
+            context.Set<T>().Add(entity);
+
+            if (count % batchSize == 0)
+            {
+                context.SaveChanges();
+
+                Debug.WriteLine($"Saved {count} entities.");
+            }
+            return context;
+        }
+    }
+}
