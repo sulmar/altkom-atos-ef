2165e515da8990679cac3beebc718cf5cfabfd64
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-04-22 13:08:00 +0200

    Tasks

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -10,7 +10,9 @@ using System.Data.Common;
 using System.Data.Entity;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleClient
@@ -21,8 +23,22 @@ namespace ConsoleClient
         {
             Console.WriteLine("Hello World!");
 
-            
-            UpdateCustomerTest();
+            // SyncTest();
+
+            // TaskTest();
+
+
+            // SyncResultTest();
+
+            Task.Run(()=>AsyncAwaitTest());
+
+
+            Console.WriteLine("Add customer!");
+
+           // TaskResultTest();
+
+
+            //  UpdateCustomerTest();
 
             // RemoveCustomerTest();
 
@@ -46,13 +62,141 @@ namespace ConsoleClient
 
             //AddOrderTest();
 
-             
+
 
             // UpdateCustomerTest();
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
 
+        }
+
+        private static void SyncResultTest()
+        {
+            int size1 = Download("http://www.google.com");
+            Send($"Pobrano {size1}");
+
+            int size2 = Download("http://www.microsoft.com");
+            Send($"Pobrano {size2}");
+
+            int size3 = Download("http://www.atos.net");
+            Send($"Pobrano {size3}");
+        }
+
+        private static async void AsyncAwaitTest()
+        {
+            int size1 = await DownloadAsync("http://www.google.com");            
+            await SendAsync($"Pobrano {size1}");
+
+            int size2 = await DownloadAsync("http://www.microsoft.com");
+            await SendAsync($"Pobrano {size2}");
+
+            int size3 = await DownloadAsync("http://www.atos.net");
+            await SendAsync($"Pobrano {size3}");
+        }
+
+        private static void TaskContinueWithTest()
+        {
+            Task.Run(() => Download("http://www.google.com"))
+                .ContinueWith(t => Send($"Pobrano {t.Result}"));
+
+            Task.Run(() => Download("http://www.microsoft.com"))
+                .ContinueWith(t => Send($"Pobrano {t.Result}"));
+
+            Task.Run(() => Download("http://www.atos.net"))
+                .ContinueWith(t => Send($"Pobrano {t.Result}"));
+        }
+
+       
+
+        private static void TaskResultTest()
+        {
+            Task<int> t1 = Task<int>.Run(() => Download("http://www.google.com"));
+
+            int size1 = t1.Result; // blokuje UI
+
+            Task.Run(()=>Send($"Pobrano {size1}"));
+
+            Task<int> t2 = Task<int>.Run(() => Download("http://www.microsoft.com"));
+
+            int size2 = t2.Result;
+
+            Task.Run(()=>Send($"Pobrano {size2}"));
+
+            Task<int> t3 = Task<int>.Run(() => Download("http://www.atos.net"));
+
+           int size3 = t3.Result;
+
+            Task.Run(()=>Send($"Pobrano {size3}"));
+
+        }
+
+       
+        private static Task<int> DownloadAsync(string url)
+        {
+            return Task.Run(() => Download(url));
+        }
+
+        private static int Download(string url)
+        {
+            Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} downloading {url}...");
+
+            WebClient client = new WebClient();
+            string content = client.DownloadString(url);
+
+            Thread.Sleep(TimeSpan.FromSeconds(3));
+
+            Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} downloaded.");
+
+            return content.Length;
+        }
+
+        private static void SyncTest()
+        {
+            Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} main");
+
+            Send("Hello World!");
+
+            Send("Hello EF!");
+
+            Send("Hello .NET!");
+
+            Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} main");
+        }
+
+        private static void TaskTest()
+        {
+            Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} main");
+
+            for (int i = 0; i < 100; i++)
+            {
+                //Task task1 = new Task(() => Send("Hello World!"));
+                //task1.Start();
+
+                Task.Run(() => Send("Hello World!"));
+            }
+            
+            //Task task2 = new Task(() => Send("Hello EF!"));
+            //Task task3 = new Task(() => Send("Hello .NET!"));
+            
+            //task2.Start();
+            //task3.Start();
+
+            Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} main");
+        }
+
+        private static Task SendAsync(string message)
+        {
+            return Task.Run(() => Send(message));
+        }
+
+        private static void Send(string message)
+        {            
+            Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} Sending {message}...");
+
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+
+            Console.WriteLine($"#{Thread.CurrentThread.ManagedThreadId} Sent.");
         }
 
         private static void GetProductByBarCodeTest()
